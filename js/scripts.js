fea483d0c1f6afbdeca006bd2d0640d2ba7fce16
Katy Roberts <letha.lillie@gmail.com>
2018-09-24 00:25:20 -0700

    semi-completed draft

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,68 @@
-var add = function(number1, number2){
-	return number1 + number2;
-};
-var subtract = function(number1, number2){
-	return number1 - number2;
-};
-var multiply = function(number1, number2){
-	return number1 * number2;
-};
-var divide = function(number1, number2){
-	return number1 / number2;
-};
 
 $(document).ready(function() {
-  $("form#calculator").submit(function() {
+  $("form#holiday").submit(function() {
 		event.preventDefault();
-    var motive = $("#input1").val();
-    var number2 = parseInt($("#input2").val());
-		var operator = $("input:radio[name=operator]:checked").val();
-		var result;
-		if (operator === "add"){
-			result = add(number1, number2);
-		} else if (operator === "subtract"){
-			result = subtract(number1, number2);
-    } else if (operator === "multiply") {
-      result = multiply(number1, number2);
-    } else if (operator === "divide") {
-      result = divide(number1, number2);
+
+    var q1 = $('input[name="q1"]:checked').val();
+    var q2 = $('input[name="q2"]:checked').val();
+    var q3 = $('input[name="q3"]:checked').val();
+    var q4 = $('input[name="q4"]:checked').val();
+    var q5 = $('input[name="q5"]:checked').val();
+    var greeceCount = 0;
+    var irelandCount = 0;
+    var portugalCount = 0;
+    if (q1 === "greece") {
+      greeceCount++;
+    } else if (q1 === "ireland") {
+      irelandCount++;
+    } else {
+      portugalCount++;
     }
-    $("#output").text(result);
+
+    if (q2 === "greece") {
+      greeceCount++;
+    } else if (q2 === "ireland") {
+      irelandCount++;
+    } else {
+      portugalCount++;
+    }
+
+    if (q3 === "greece") {
+      greeceCount++;
+    } else if (q3 === "ireland") {
+      irelandCount++;
+    } else {
+      portugalCount++;
+    }
+    if (q4 === "greece") {
+      greeceCount++;
+    } else if (q4 === "ireland") {
+      irelandCount++;
+    } else {
+      portugalCount++;
+    }
+    if (q5 === "greece") {
+      greeceCount++;
+    } else if (q5 === "ireland") {
+      irelandCount++;
+    } else {
+      portugalCount++;
+    }
+
+    if (greeceCount > 2 || irelandCount > 2 || portugalCount > 2) {
+      if (greeceCount > 2) $("#output").text("You are off to beautiful GREECE!!")
+      else if (irelandCount > 2) $("#output").text("You are off to the Emerald Isle!")
+      else if (portugalCount > 2) $("#output").text("You will enjoy a magnificent adventure in Portugal.")
+      
+    } else {
+      if (greeceCount >= 2) $("#output").text("You are off to beautiful GREECE!!")
+      else if (irelandCount >= 2) $("#output").text("You are off to the Emerald Isle!")
+      else if (portugalCount >= 2)
+      $("#output").text("You will enjoy a magnificent adventure in Portugal.")
+
+    }
+
+
 
 	});
 });
-
-// 		var result1 = add(number1, number2);
-// 		$(".result1").text(result1);
-// 		var result2 = subtract(number3, number4);
-// 		$(".result2").text(result2);
-// 		var result3 = multiply(number5, number6);
-// 		$(".result3").text(result3);
-// 		var result4 = divide(number7, number8);
-// 		$(".result4").text(result4);
-//
-// 		$(".feedback").show();
-// 		var feeling = $("input:radio[name=feeling]:checked").val();
-//
-// 		event.preventDefault();
-//   });
-//
-//
-//
-//
-// });
